2955bfd468f850ab0eb6c66ae875ae9df73c22b1
Hamza <hamza01961@gmail.com>
2021-09-17 12:40:07 +0500

    login done

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
         <Nav/>
-        <Login/>      
+        {/* <Login/>       */}
     </div>
   );
 }
--- Client/src/components/Login/Login.tsx
+++ Client/src/components/Login/Login.tsx
@@ -1,12 +1,15 @@
 import './login.css'
+import {useState} from 'react'
 
 function Login() {
+    const [email,setEmail] = useState<string>('')
+    const [password,setPassword] = useState<string>('')
     return (
         <div className="main">
             <p className="sign" >Sign in</p>
             <form className="form1">
-                <input className="un " type="text" placeholder="Email" />
-                <input className="un" type="password" placeholder="Password" />
+                <input className="un " type="text" placeholder="Email" onChange={(e)=>{setEmail(e.target.value)}}/>
+                <input className="un" type="password" placeholder="Password" onChange={(e)=>{setPassword(e.target.value)}}/>
                 <button className="submit" >Sign in</button> 
             </form>
         </div>
