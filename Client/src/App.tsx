dba6841bfdfd9369d9c970475f5d4f8dbce3527d
Hamza <hamza01961@gmail.com>
2021-09-17 22:25:08 +0500

    items table done

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,11 +2,13 @@ import './App.css';
 import Login from './components/Login/Login'
 import Nav from './components/Nav/Nav';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Items from './components/Items/Items';
 
 function App() {
   return (
     <div className="App">
         <Nav/>
+        <Items/>
         {/* <Login/>       */}
     </div>
   );
