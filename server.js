24ac5cff5c6646a461412818d936c700227ba43c
Hamza <hamza01961@gmail.com>
2021-11-27 18:27:19 +0500

    build

--- server.js
+++ server.js
@@ -11,17 +11,18 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
+app.use(express.static('./Client/build'))
 
 const PORT = process.env.PORT || 8000
 
 const supabase = createClient('https://yvwetawhxztjsjgaxuon.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNTQ4NTU4NSwiZXhwIjoxOTUxMDYxNTg1fQ.OApMwM_MiVg33icr0Y6Llzw-dl9sIC8rHY2D2g9MKZw')
 global.supabase=supabase
 
+
 app.listen(PORT,()=>{
     console.log("server is live...")
-        
+    
 })
-
 app.use('/api/login',handleLogin)
 app.use('/api/Item',handleItem)
 app.use('/api/Cafetarian',handleCafetarian)
