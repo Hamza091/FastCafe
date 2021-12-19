2d6ed85d747e22d0531eeec11c1c4c2d6d592b55
Hamza <hamza01961@gmail.com>
2021-12-19 22:04:37 +0500

    done

--- Controller/Items/AddItem.js
+++ Controller/Items/AddItem.js
@@ -15,14 +15,19 @@ async function addItem(req,res){
             
             if(itemDetails.item_category.length>0){
                 const id = item.data[0].item_id
-
-                itemDetails.item_category.map((itm)=>{
-                    itm.item_id=id
-                })
+                let itm_category = []
+                for(i=0;i<itemDetails.item_category.length;i++){
+                    itm_category = [...itm_category,{"cat_id":itemDetails.item_category[i].cat_id,"item_id":id}]
+                }
+                console.log(itm_category)
+                cat = await db.from('item_category').insert(itm_category)
+                // itemDetails.item_category.map((itm)=>{
+                //     itm.item_id=id
+                // })
                 
-                console.log("here")
-                console.log(itemDetails)
-                cat = await db.from('item_category').insert(itemDetails.item_category)
+                // console.log("here")
+                // console.log(itemDetails)
+                // cat = await db.from('item_category').insert(itemDetails.item_category)
             }
         
     }
